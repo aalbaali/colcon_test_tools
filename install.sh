33c8d2696665843749b05dac9400f087ca08088a
aalbaali <albaalia@live.com>
2022-03-01 13:43:14 +0000

    Add installation script

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Copy installation file to installation directory
+cp colcon_tools.sh /usr/local/bin
+
+# Installation script sources `run_tests` into `.bashrc`, if `.bashrc` doesn't do that already
+if [ -n $(cat ~/.bashrc | grep "colcon_tools.sh")]
+then
+  echo "source /usr/local/bin/colcon_tools.sh" >> ~/.bashrc
+fi
